a36225c3627f7f14e737c65c50d6656a7c98d137
dynaworks software <dynaworks.software@gmail.com>
2025-07-30 14:41:30 -0600

    some changes

--- app/main.py
+++ app/main.py
@@ -3,8 +3,6 @@
 from app.config import AppSettings, DBSettings
 # Logging imports
 from loguru import logger
-# Jsend for structured responses
-import jsend
 # Database imports
 from app.database.db import DBConnectionPool
 # FastAPI imports
@@ -15,44 +13,65 @@ from contextlib import asynccontextmanager
 from app.routes.system import router as system_router
 from app.routes.data_process_type import router as data_process_type_router
 
-# Connect to the database and return the connection pool.
-def connect_to_db():
-    try:
-        # Load database settings from the environment
-        db_settings = DBSettings()
 
-        # Validate the database settings    
-        if not db_settings.is_valid():
-            raise ValueError("Invalid database settings provided.")
-        
-        # Log the connection details
-        logger.info(f"Connecting to database {db_settings.dbname} at {db_settings.host}:{db_settings.port}")
-        
-        db_conn = DBConnection(user=db_settings.user, password=db_settings.password, host=db_settings.host, \
-                                 port=db_settings.port, dbname=db_settings.dbname, appname=db_settings.appname, \
-                                 min_size=db_settings.min_size, max_size=db_settings.max_size, timeout=DBSettings.timeout_conn)
+# Create database connection pool
+def create_db_pool():
+    """
+    Create a database connection pool using the provided settings.
+    """
+    # Load database settings from the environment
+    db_settings = DBSettings()
+
+    # Validate the database settings    
+    if not db_settings.is_valid():
+        raise ValueError("Invalid database settings provided.")
+    
+    # Log the connection details
+    logger.info(f"Connecting to database {db_settings.dbname} at {db_settings.host}:{db_settings.port}")
+
+    # Create a database connection pool
+    db_conn = DBConnectionPool(user=db_settings.user, password=db_settings.password, host=db_settings.host, \
+                                    port=db_settings.port, dbname=db_settings.dbname, appname=db_settings.appname, \
+                                    min_size=db_settings.min_size, max_size=db_settings.max_size, timeout=DBSettings.timeout_conn)
+
+    return db_conn
+
+# Initialize the database connection pool
+try:
+    db_conn = create_db_pool()
+except Exception as e:
+    logger.error(f"Failed to create database connection pool: {e}")
+    raise RuntimeError(f"Database connection pool creation failed: {e}")
+
+# Connect to the database and return the connection pool.
+def connect_to_db(db_conn: DBConnectionPool):
+    try:
         # Connect to the database        
         logger.info("Creating database connection...")
         db_conn.connect(timeout=DBSettings.timeout_conn)
         db_version = db_conn.test()
         # Log the successful connection
         logger.info(f"Database connection established successfully. Version: {db_version}")
-        # Return the connection pool
-        return db_conn
+        return
     except Exception as e:
         raise e  # Re-raise the exception to be handled in the lifespan event
 
+
 # FastAPI lifespan events
 @asynccontextmanager
-async def lifespan(app: FastAPI, dependencies=[Depends(connect_to_db)]):
-       # Startup lifespan event handler
+async def lifespan(app: FastAPI):
+    # Startup lifespan event handler
     logger.info(f"Starting up {app.title} v{app.version} application...")
     
     # Connect to the database
-    db_conn = connect_to_db()
-    if db_conn is None:
-        logger.error("Failed to connect to the database during startup.")
-        raise RuntimeError("Database connection failed")
+    try:
+        connect_to_db(db_conn)
+        if db_conn is None:
+            logger.error("Failed to connect to the database during startup.")
+            raise RuntimeError("Database connection failed")
+    except Exception as e:
+        logger.error(f"Error occurred during database connection: {e}")
+        raise RuntimeError(f"Database connection failed during startup: {e}")
     
     # Store the connection pool in the app state
     app.state.db_conn = db_conn
@@ -78,11 +97,10 @@ async def lifespan(app: FastAPI, dependencies=[Depends(connect_to_db)]):
     except Exception as e:
         logger.error(f"Error closing connection pool: {e}")
 
+    return
 
 # Create FastAPI app
-app = FastAPI(lifespan=lifespan, \
-            title=AppSettings.name, version=AppSettings.version, \
-            description=AppSettings.description)
+app = FastAPI(lifespan=lifespan, title=AppSettings.name, version=AppSettings.version, description=AppSettings.description)
 
 # Include routers
 app.include_router(system_router, prefix=AppSettings.base_url)
@@ -91,12 +109,4 @@ app.include_router(data_process_type_router, prefix=AppSettings.base_url)
 # Root endpoint for health check
 @app.get("/")
 async def root():
-    return jsend.success({'status':'OK'})
-
-
-
-
-
-
-
-
+    return ({'App Name': AppSettings.name, 'Version': AppSettings.version, 'Description': AppSettings.description})
--- app/routes/system.py
+++ app/routes/system.py
@@ -4,8 +4,7 @@
 # 
 # FastAPI imports
 from fastapi import FastAPI, APIRouter, Depends, HTTPException, Request
-# jsend is a library for creating JSON responses with a consistent structure
-import jsend
+
 
 # Create FastAPI router for system-related endpoints
 router = APIRouter(tags=["system"])
@@ -15,11 +14,11 @@ router = APIRouter(tags=["system"])
 async def db_version(request: Request):
     # Get the database version
     db_version= request.app.state.db_conn.test()
-    return jsend.success({f"db_version": {db_version}})
+    return {f"db_version": {db_version}}
 
 # Get database stats
 @router.get("sys/dbinfo", summary="Get Database connection Stats")
 async def db_conn_info(request: Request):
     # Get the database connection pool from the app state
     pool = request.app.state.db_conn.pool
-    return jsend.success({f"connection_stats": {str(pool.get_stats())}})
+    return {f"connection_stats": {str(pool.get_stats())}}
